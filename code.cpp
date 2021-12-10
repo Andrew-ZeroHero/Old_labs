73bd44a1646edf233de0032b7fa0bcd2a62f36ca
Дрон <93979827+Andrew-ZeroHero@users.noreply.github.com>
2021-12-10 10:06:32 +0400

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,46 +1,64 @@
-#include <iostream>
-#include <stdlib.h>
-#include <windows.h>
-#include <vector>
-#include <algorithm>
+#include <stdio.h>
 #include <locale>
-#include <conio.h>
-#include <fstream>
-using namespace std;
 
 int main()
 {
-	setlocale(LC_ALL, "RUS");
-	vector <vector<int>> gg;
-	int i, n, k, j;
-	ofstream fileo;
-	cout << "Введите ограничение: ";
-	cin >> n;
-	fileo.open("C:\\Users\\Andrey\\Documents\\Файл.txt");
-	fileo << "Вывод массива:" << endl;
-	for (i = 0; i < n; i++)
-	{
-		vector <int> a;
-		for (j = 0; j < n; j++)
-		{
-			k = rand() % 150 - 50;
-			a.push_back(k);
-		}
-		gg.push_back(a);
-	}
-	cout << "Вывод массива:" << endl;
-	for (i = 0; i < n; i++)
-	{
-		for (j = 0; j < n; j++)
-		{
-			std::cout.width(4);
-			std::cout << gg[i][j];
-			fileo.width(4);
-			fileo << gg[i][j];
-		}
-		std::cout << endl;
-		fileo << endl;
-	}
-	fileo << endl << endl;
-	system("pause");
+    setlocale(LC_ALL, "RUS");
+    int n, m, a[255][255], k = 0;
+    printf("Размер матрицы m*n(>0):\n");
+    do
+    {
+        printf("n = ");
+        scanf_s("%d", &n);
+        printf("m = ");
+        scanf_s("%d", &m);
+        if (n <= 0||m <= 0)
+        {
+            if (m <= 0)
+            {
+                printf("Неверное значение m\n");
+            }
+            if (n <= 0)
+            {
+                printf("Неверное значение n\n");
+            }
+            printf("Введите значения заново!\n");
+        }
+    } while (n <= 0 || m <= 0 || n > 255 || m > 255);
+            printf("Ввод матрицы(1 и 0):\n");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    do
+                    {
+                        printf("a[%d][%d] = ", i, j);
+                        scanf_s("%d", &a[i][j]);
+                        if (a[i][j] != 0 && a[i][j] != 1)
+                        {
+                            printf("Введите значения заново (0 или 1)!\n\n");
+                        }
+                    } while (a[i][j] != 0 && a[i][j] != 1);
+                }
+                printf("\n");
+            }
+            printf("\n\n\n");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    printf(" %4d", a[i][j]);
+                    if (a[i][j] == 0 && a[i][j + 1] == 1 && a[i][j + 2] == 0 && a[i + 1][j] == 1 && a[i + 1][j + 1] == 0 && a[i + 1][j + 2] == 1 && a[i + 2][j] == 0 && a[i + 2][j + 1] == 1 && a[i + 2][j + 2] == 0)
+                    {
+                        k++;
+                    }
+                }
+                printf("\n\n\n");
+            }
+            printf("Всего фрагментов заданного вида\n");
+            printf(" 0  1  0 \n\n"); 
+            printf(" 1  0  1 \n\n");
+            printf(" 0  1  0 \n\n\n");
+            printf(" %d\n\n", k);
+            system("pause");
 }
