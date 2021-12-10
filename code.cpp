9194c487b507877c2e02e68b4e374450127896f8
Дрон <93979827+Andrew-ZeroHero@users.noreply.github.com>
2021-12-10 10:08:13 +0400

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,46 +1,112 @@
 #include <iostream>
-#include <stdlib.h>
 #include <windows.h>
-#include <vector>
-#include <algorithm>
-#include <locale>
+#include <math.h>
 #include <conio.h>
-#include <fstream>
+#include <locale>
 using namespace std;
-
 int main()
 {
 	setlocale(LC_ALL, "RUS");
-	vector <vector<int>> gg;
-	int i, n, k, j;
-	ofstream fileo;
-	cout << "Введите ограничение: ";
+	float x=10;
+	int n, k=0, l=0, m=0;
+	HDC hDC = GetDC(GetConsoleWindow());
+	cout << "Выберите фигуру(1-ромб, 2-куб, 3-трапеция): ";
 	cin >> n;
-	fileo.open("C:\\Users\\Andrey\\Documents\\Файл.txt");
-	fileo << "Вывод массива:" << endl;
-	for (i = 0; i < n; i++)
+	if (n == 1)
 	{
-		vector <int> a;
-		for (j = 0; j < n; j++)
-		{
-			k = rand() % 150 - 50;
-			a.push_back(k);
-		}
-		gg.push_back(a);
+		HPEN Pen1 = CreatePen(PS_SOLID, 2, RGB(255, 255, 0));
+		SelectObject(hDC, Pen1);
+		MoveToEx(hDC, x*x, x*x, NULL);
+		LineTo(hDC, x*x + 50, x*x + 100);
+		LineTo(hDC, x*x, x*x + 200);
+		LineTo(hDC, x*x - 50, x*x + 100);
+		LineTo(hDC, x*x, x*x);
 	}
-	cout << "Вывод массива:" << endl;
-	for (i = 0; i < n; i++)
+	else
 	{
-		for (j = 0; j < n; j++)
+		if (n == 2)
 		{
-			std::cout.width(4);
-			std::cout << gg[i][j];
-			fileo.width(4);
-			fileo << gg[i][j];
+			HPEN Pen2 = CreatePen(PS_SOLID, 2, RGB(255, 0, 255));
+			SelectObject(hDC, Pen2);
+			MoveToEx(hDC, x*x, x*x, NULL);
+			LineTo(hDC, x*x +400, x*x); 
+			LineTo(hDC, x*x +400, x*x +200); 
+			LineTo(hDC, x*x +360, x*x +240); 
+			LineTo(hDC, x*x -40, x*x +240); 
+			LineTo(hDC, x*x -40, x*x +40); 
+			LineTo(hDC, x*x, x*x);
+			MoveToEx(hDC, x*x - 40, x*x + 40, NULL);
+			LineTo(hDC, x*x +360, x*x +40);
+			LineTo(hDC, x*x + 400, x*x);
+			MoveToEx(hDC, x*x + 360, x*x + 40, NULL);
+			LineTo(hDC, x*x + 360, x*x + 240);
+			MoveToEx(hDC, x*x, x*x, NULL);
+			LineTo(hDC, x*x, x*x + 200);
+			LineTo(hDC, x*x - 40, x*x + 240);
+			MoveToEx(hDC, x*x, x*x + 200, NULL);
+			LineTo(hDC, x*x + 400, x*x + 200);
 		}
-		std::cout << endl;
-		fileo << endl;
+		else
+		{
+			if (n == 3)
+			{
+				HPEN Pen3 = CreatePen(PS_SOLID, 2, RGB(0, 255, 255));
+				SelectObject(hDC, Pen3);
+				MoveToEx(hDC, x*x, x*x, NULL);
+				LineTo(hDC, x*x + 200, x*x);
+				LineTo(hDC, x*x + 250, x*x + 150);
+				LineTo(hDC, x*x - 50, x*x + 150);
+				LineTo(hDC, x*x, x*x);
+			}
+			else
+			{
+				if (n = 1337) //Пасхалочка на 1337
+				{
+					HPEN Pen4 = CreatePen(PS_SOLID, 5, RGB(255, 255, 255));
+					SelectObject(hDC, Pen4);
+					MoveToEx(hDC, x*x, x*x, NULL);
+					LineTo(hDC, x*x +200, x*x);
+					MoveToEx(hDC, x*x + 100, x*x, NULL);
+					LineTo(hDC, x*x + 100, x*x +250);
+					MoveToEx(hDC, x*x + 220, x*x, NULL);
+					LineTo(hDC, x*x + 220, x*x + 250);
+					LineTo(hDC, x*x + 299, x*x + 250);
+					LineTo(hDC, x*x + 299, x*x + 200);
+					LineTo(hDC, x*x + 220, x*x + 200);
+					MoveToEx(hDC, x*x + 320, x*x, NULL);
+					LineTo(hDC, x*x + 320, x*x + 250);
+					MoveToEx(hDC, x*x + 400, x*x +250, NULL);
+					LineTo(hDC, x*x + 400, x*x);
+					LineTo(hDC, x*x + 500, x*x);
+					LineTo(hDC, x*x + 500, x*x +250);
+					MoveToEx(hDC, x*x + 520, x*x, NULL);
+					LineTo(hDC, x*x + 520, x*x +250);
+					LineTo(hDC, x*x + 620, x*x);
+					LineTo(hDC, x*x + 620, x*x +250);
+					MoveToEx(hDC, x*x + 640, x*x +250, NULL);
+					LineTo(hDC, x*x + 640, x*x + 225);
+					LineTo(hDC, x*x + 740, x*x + 225);
+					LineTo(hDC, x*x + 740, x*x + 250);
+					MoveToEx(hDC, x*x + 665, x*x + 225, NULL);
+					LineTo(hDC, x*x + 665, x*x);
+					LineTo(hDC, x*x + 715, x*x);
+					LineTo(hDC, x*x + 715, x*x + 225);
+					MoveToEx(hDC, x*x + 760, x*x, NULL);
+					LineTo(hDC, x*x + 760, x*x + 250);
+					LineTo(hDC, x*x + 860, x*x + 250);
+					LineTo(hDC, x*x + 860, x*x);
+					LineTo(hDC, x*x + 760, x*x);
+					MoveToEx(hDC, x*x + 880, x*x +250, NULL);
+					LineTo(hDC, x*x + 880, x*x);
+					LineTo(hDC, x*x + 950, x*x);
+					LineTo(hDC, x*x + 950, x*x +100);
+					LineTo(hDC, x*x + 880, x*x +100);
+				}
+				else
+					cout << "Введено неверное значение!" << endl;
+			}
+		}
 	}
-	fileo << endl << endl;
-	system("pause");
+	_getch();
+	return 0;
 }
